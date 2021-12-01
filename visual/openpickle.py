5a6a5c88116a545d790493bd6067322b080a2e79
Dong Wang <dongwang@DongdeMBP.paxio.net>
2021-12-01 13:49:12 -0800

    process feature files

--- visual/openpickle.py
+++ visual/openpickle.py
@@ -1,24 +1,43 @@
 import pickle
+import os
 
-with open("trn.txt") as info:
-    lines = info.readlines()
+def processFeatureFiles(category = "trn"):
+    filename = category+".txt"
+    with open(filename) as info:
+        lines = info.readlines()
 
-names = {}
-for line in lines:
-    key = line.split(","[1]) # word
-    value = []
+    fnToWord = {}
+    for line in lines:
+        lineComponents = line.split(",")
+        key = lineComponents[0] # filename
+        value = lineComponents[1] # word
+        if key not in fnToWord.keys():
+            fnToWord[key] = value
+        else:
+            print("ERROR: duplicate filename:", key)
+    f = open('feature.pkl', 'rb')
+    # format: ('filename', tensor feature matrix)
+    while True:
+        try:
+            data = pickle.load(f)
+            # if in trn.txt
+            filename = data[0] # filename
+            if filename in fnToWord.keys():
+                # get feature and save in the folder with "filename" and the current filename should be a counting number
+                word = fnToWord[filename] # new key
+                try:
+                    if not os.path.exists(word):
+                        os.makedirs(word)
+                except OSError:
+                    print("ERROR: failed to create dir: ", word)
+                
+                feat = data[1]
+                featFile = open(word+'/'+category+'/'+filename+'.pkl', 'wb')
+                pickle.dump(feat, featFile)
+                featFile.close()
+                break
 
-f = open('feature.pkl', 'rb')
-# format: ('filename', tensor feature matrix)
+        except EOFError:
+            break
 
-while True:
-    try:
-        data = pickle.load(f)
-        # if in trn.txt
-        pdir = data[0]
-        if pdir in names:
-            # get feature and save in the folder with "filename" and the current filename should be a counting number
-            feat = data[1]
 
-    except EOFError:
-        break
