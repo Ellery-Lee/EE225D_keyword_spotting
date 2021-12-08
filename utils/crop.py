5dcd7e125997b6aa20fd864e4a8791ba18c1a06c
Wdong04 <dongwang@DongdeMBP.paxio.net>
2021-12-08 13:27:01 -0800

    crop info txt

--- utils/crop.py
+++ utils/crop.py
@@ -1,13 +1,21 @@
+# coding: utf-8
 dir = '/Users/dongwang/Desktop/225D/project/EE225D_keyword_spotting/config/lrw1000/'
 filename = "all_audio_video"
 ext = ".txt"
 with open(dir+filename+ext, 'r') as f:
     lines = f.readlines()
+with open("/Users/dongwang/Desktop/225D/project/EE225D_keyword_spotting/config/lrw1000/first50.txt") as df:
+    dflines = df.readlines()
 
+dict = []
+for line in dflines:
+    word = line.strip('\n')
+    dict.append(word)
+    
 nf = open(filename+"_20"+ext, 'w')
 
 for line in lines:
-    if line.split(',')[0] == '19e5c24d226c1f8f4bd2490db172649e' and line.split(',')[1] == 'd31e32048b51f66775ae2be3636a52ac':
-        break
-    nf.write(line)
+    if line.split(',')[2] in dict:
+        nf.write(line)
+
 nf.close()
